762e1ca7480d92157783b7f4c7f32d69c86bc16e
Veselin <veselin271102@gmail.com>
2026-05-09 18:49:41 +0300

    H1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar">
-      <h2>Pamporovo</h2>
+      <h2>Пампорово</h2>
       <div>
         <Link to="/">Home</Link>
         <Link to="/summer">Summer</Link>
